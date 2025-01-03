a8ecf6fd00eaeb14101fc05998fc24bbf3d441fb
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-01-03 23:51:54 +0530

    jenkins ec2 instances

---
+++ install_jenkins_agent.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#resize disk from 20GB to 50GB
+growpart /dev/nvme0n1 4
+
+lvextend -L +10G /dev/mapper/RootVG-homeVol
+lvextend -L +10G /dev/mapper/RootVG-varVol
+lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
+
+xfs_growfs /home
+xfs_growfs /var/tmp
+xfs_growfs /var
+
+yum install fontconfig java-17-openjdk -y
+
+#This repository We are using for Jenkins only. So not below installation steps.
+
+# #Installing Terraform on Jenkins Agent
+# yum install -y yum-utils
+# yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+# yum -y install terraform
+
+
+# #Node JS installation
+# dnf module disable nodejs -y
+# dnf module enable nodejs:20 -y
+# dnf install nodejs -y
+
+# #Installing zip in Jenins Agent
+# yum install zip -y
+
+
+# # docker
+# yum install -y yum-utils
+# yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+# yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# systemctl start docker
+# systemctl enable docker
+# usermod -aG docker ec2-user
---
+++ main.tf
@@ -0,0 +1,160 @@
+module "jenkins_master" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  name = "tf-jenkins-master"
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-06b1b57b365846051"] #replace your SG
+   ami                   = data.aws_ami.ami_info.id
+  #ami                    = "ami-041e2ea9402c46c32"
+  #user_data              = file("install_jenkins_master.sh")
+  user_data               = file("${path.module}/install_jenkins_master.sh")
+
+  tags = {
+    Name   = "Jenkins-Master"
+  }
+  # Define the root volume size and type
+  root_block_device = [
+    {
+      volume_size = 50       # Size of the root volume in GB
+      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+    }
+  ]
+
+}
+module "jenkins_agent" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "tf-jenkins-agent"
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-06b1b57b365846051"] #replace your SG
+  ami                   = data.aws_ami.ami_info.id
+  #ami                    = "ami-041e2ea9402c46c32"
+  #user_data              = file("install_jenkins_agent.sh")
+  user_data               = file("${path.module}/install_jenkins_agent.sh")
+
+  tags = {
+    Name   = "Jenkins-Agent"
+  }
+  # Define the root volume size and type
+  root_block_device = [
+    {
+      volume_size = 50       # Size of the root volume in GB
+      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+    }
+  ]
+
+}
+
+# The tools key is generated for nexus server.
+# resource "aws_key_pair" "tools" {
+#     key_name = "tools-key"
+#     #you can paste the public key directly like this
+#     #public_key = file("~/.ssh/openssh.pub")
+#     # ~ means windows home directory
+#     public_key = "${file("~/.ssh/tools.pub")}"
+
+# }
+
+# module "nexus" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#   name = "nexus"
+
+#   instance_type          = "t3.medium"
+#   vpc_security_group_ids = ["sg-06b1b57b365846051"] #replace your SG
+#   ami                   = data.aws_ami.nexus_ami_info.id
+#   key_name = aws_key_pair.tools.key_name
+   
+#     root_block_device = [
+#     {
+#       volume_type = "gp3"
+#       volume_size = 50
+#     }
+#     ]
+  
+#   tags = {
+#     Name   = "Nexus"
+#   }
+# }
+# module "sonarqube" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#   name = "sonarqube"
+
+#   instance_type          = "t3.medium"
+#   vpc_security_group_ids = ["sg-06b1b57b365846051"] #replace your SG
+#   ami                   = data.aws_ami.sonarqube_ami_info.id
+#   #ami                   = "ami-0649f08ef033b0cc2"
+#   key_name = aws_key_pair.tools.key_name
+   
+#     root_block_device = [
+#     {
+#       volume_type = "gp3"
+#       volume_size = 50
+#     }
+#     ]
+  
+#   tags = {
+#     Name   = "SonarQube"
+#   }
+# }
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+  zone_name = var.zone_name
+
+records = [
+      {
+        name = "jenkins_master"
+        type = "A"
+        ttl  = 1
+        records = [
+          module.jenkins_master.public_ip
+        ]
+       # allow_overwrite = true
+      },
+      {
+        name = "jenkins_agent"
+        type = "A"
+        ttl  = 1
+        records = [
+          module.jenkins_agent.public_ip
+        ]
+        #allow_overwrite = true
+       }
+       #,
+      #   {
+      #   name = "nexus"
+      #   type = "A"
+      #   ttl  = 1
+      #   records = [
+      #     module.nexus.public_ip
+      #   ]
+      #   #allow_overwrite = true
+      # },
+      # {
+      #   name = "sonarqube"
+      #   type = "A"
+      #   ttl  = 1
+      #   records = [
+      #     module.sonarqube.public_ip
+      #   ]
+      #   #allow_overwrite = true
+      # }
+   ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
