6ed92d963c91e161206bc26efcf404c1346c19ef
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-02 12:06:41 +0530

    jenkins master agents ec2

--- install_jenkins_master.sh
+++ install_jenkins_master.sh
@@ -2,16 +2,18 @@
 curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-17-openjdk jenkins -y
-systemctl daemon-reload
-systemctl enable jenkins
-systemctl start jenkins
-
 
 #resize disk from 20GB to 50GB
 growpart /dev/nvme0n1 4
+
 lvextend -L +10G /dev/RootVG/rootVol
 lvextend -L +10G /dev/mapper/RootVG-varVol
 lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
+
 xfs_growfs /
 xfs_growfs /var/tmp
 xfs_growfs /var
+
+systemctl daemon-reload
+systemctl enable jenkins
+systemctl start jenkins
--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "jenkins_master" {
   instance_type          = "t3.small"
   vpc_security_group_ids = [var.allow_everything] #replace your SG
   ami                    = data.aws_ami.ami_info.id
+  #vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
   user_data              = file("${path.module}/install_jenkins_master.sh")
   tags = {
     Name   = "Jenkins-Master"
@@ -24,6 +25,7 @@ module "jenkins_agent" {
   instance_type          = "t3.small"
   vpc_security_group_ids = [var.allow_everything] #replace your SG
   ami                    = data.aws_ami.ami_info.id
+    #vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
   user_data               = file("${path.module}/install_jenkins_agent.sh")
 
   tags = {
@@ -37,61 +39,7 @@ module "jenkins_agent" {
       delete_on_termination = true  # Automatically delete the volume when the instance is terminated
     }
   ]
-
 }
-
-# #The tools key is generated for nexus server.
-# resource "aws_key_pair" "tools" {
-#     key_name = "tools-key"
-#     #you can paste the public key directly like this
-#     #public_key = file("~/.ssh/openssh.pub")
-#     # ~ means windows home directory
-#     public_key = "${file("~/.ssh/tools.pub")}"
-
-# }
-
-# module "nexus" {
-#   source  = "terraform-aws-modules/ec2-instance/aws"
-#   name = "nexus"
-
-#   instance_type          = "t3.medium"
-#   vpc_security_group_ids = [var.allow_everything] #replace your SG
-#   ami                    = data.aws_ami.nexus_ami_info.id
-#   key_name               = aws_key_pair.tools.key_name
-   
-#     root_block_device = [
-#     {
-#       volume_type = "gp3"
-#       volume_size = 50
-#     }
-#     ]
-  
-#   tags = {
-#     Name   = "Nexus"
-#   }
-# }
-# module "sonarqube" {
-#   source  = "terraform-aws-modules/ec2-instance/aws"
-#   name = "sonarqube"
-
-#   instance_type          = "t3.medium"
-#   vpc_security_group_ids = [var.allow_everything] #replace your SG
-#   ami                   = data.aws_ami.sonarqube_ami_info.id
-#   #ami                   = "ami-0649f08ef033b0cc2"
-#   key_name = aws_key_pair.tools.key_name
-   
-#     root_block_device = [
-#     {
-#       volume_type = "gp3"
-#       volume_size = 50
-#     }
-#     ]
-  
-#   tags = {
-#     Name   = "SonarQube"
-#   }
-# }
-
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -105,7 +53,7 @@ records = [
         records = [
           module.jenkins_master.public_ip
         ]
-       # allow_overwrite = true
+         allow_overwrite = true
       },
       {
         name = "jenkins_agent"
@@ -114,26 +62,7 @@ records = [
         records = [
           module.jenkins_agent.public_ip
         ]
-        #allow_overwrite = true
+         allow_overwrite = true
        }
-      #  ,
-      #   {
-      #   name = "nexus"
-      #   type = "A"
-      #   ttl  = 1
-      #   records = [
-      #     module.nexus.public_ip
-      #   ]
-      #   #allow_overwrite = true
-      # } #,
-      # {
-      #   name = "sonarqube"
-      #   type = "A"
-      #   ttl  = 1
-      #   records = [
-      #     module.sonarqube.public_ip
-      #   ]
-      #   #allow_overwrite = true
-      # }
    ]
 }
